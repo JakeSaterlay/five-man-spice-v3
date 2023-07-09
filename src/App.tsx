8f8669973ad17b165328a618635b628b31a92686
Jake Saterlay <jakesaterlay@hotmail.co.uk>
2023-07-09 14:32:01 +0100

    logic to get random champion, set up consts and remove default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { roleList, championList } from "./constants";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [roles, setRoles] = useState<string[]>(roleList);
+	const [champions, setChampions] = useState<string[]>(championList);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const getRandomChamp = (): string => {
+		const randomChampion =
+			champions[Math.floor(Math.random() * champions.length)];
+
+		setChampions(champions.filter((x) => x !== randomChampion));
+		console.log("Random champion", randomChampion);
+		return randomChampion;
+	};
+
+	return (
+		<>
+			<button onClick={getRandomChamp}>Random Champion</button>
+		</>
+	);
 }
 
-export default App
+export default App;
