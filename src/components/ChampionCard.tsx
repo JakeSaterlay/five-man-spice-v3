f60c01ea84faac24393f76d206fbb8d4591a2355
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2023-08-28 21:27:50 +0100

    added images without styling

--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -8,6 +8,11 @@ interface Props {
 function ChampionCard({ player }: Props) {
   return (
     <div className={styles.container}>
+      <img
+        src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${player.champion}_0.jpg`}
+        height={"100%"}
+        width={"100%"}
+      />
       Hello {player.name} you are {player.champion} {player.role}
     </div>
   );
