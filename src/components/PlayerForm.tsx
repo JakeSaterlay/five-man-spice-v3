a0b2416c2177402dbe664410529034d87a36559d
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2023-09-30 09:53:48 +0100

    added tailwind css for styling

--- src/components/PlayerForm.tsx
+++ src/components/PlayerForm.tsx
@@ -20,7 +20,12 @@ function PlayerForm({ onPlayerSubmit }: Props) {
         onChange={(e) => setName(e.target.value)}
         value={name}
       />
-      <button type="submit">Submit</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounde"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
   );
 }
